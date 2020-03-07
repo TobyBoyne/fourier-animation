10a30c0c45d829f2b58fbc0d2c7ffd0bcd3d06ac
Toby Boyne <tobyboyne@gmail,.com>
2020-03-07 16:42:20 +0000

    Wrote code for animator class to plot given functions for x and y over time

--- anim.py
+++ anim.py
@@ -6,39 +6,39 @@ from matplotlib.animation import FuncAnimation
 plt.style.use('seaborn-pastel')
 
 
-
-
 def init():
 	line.set_data([], [])
 	return line,
-def animate(i):
-	x = np.linspace(0, 4, 1000)
-	y = np.sin(2 * np.pi * (x - 0.01 * i))
-	line.set_data(x, y)
-	return line,
-
 
 
 class Animator(FuncAnimation):
-	def __init__(self, fig, T):
+	def __init__(self, fig, fourier_x, fourier_y, T):
 		fps = 20
+		self.total_frames = int(T*1000 // fps)
 		kwargs = {
 			"init_func": init,
-			"frames": T // fps,
+			"frames": self.total_frames,
 			"interval": fps,
 			"blit": True
 		}
 		super().__init__(fig, self.animate, **kwargs)
+		self.T = T
+		self.f_x = fourier_x
+		self.f_y = fourier_y
+
 
 	def animate(self, i):
-		pass
+		ts = np.linspace(0, self.T * i/self.total_frames, 50)
 
-# anim = FuncAnimation(fig, animate, init_func=init,
-# 							   frames=100, interval=20, blit=True)
+		xs = self.f_x(ts)
+		ys = self.f_y(ts)
+		line.set_data(xs, ys)
+		return line,
 
 
 if __name__ == "__main__":
 	fig = plt.figure()
-	ax = plt.axes(xlim=(0, 1), ylim=(0, 1))
+	ax = plt.axes(xlim=(-1, 2), ylim=(-1, 2))
 	line, = ax.plot([], [], lw=3)
-	anim = Animator(fig, 1000)
+	anim = Animator(fig, np.sin, np.cos, 6.28)
+	plt.show()
