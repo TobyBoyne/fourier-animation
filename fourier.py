7912bfdbc1fff28ee4e58ce7abbd82c92ac7283d
Toby Boyne <tobyboyne@gmail,.com>
2020-03-07 15:17:31 +0000

    Defined __call__ method for Fourier class to evaluate value at a given value of t

--- fourier.py
+++ fourier.py
@@ -11,11 +11,12 @@ class Fourier:
 		self.a_n = np.zeros(N)
 		self.b_n = np.zeros(N)
 
-		L = points[-1, 0] - points[0, 0]
+		self.L = points[-1, 0] - points[0, 0]
 
-		self.get_poitns_for_trapz(points, L)
+		values = self.get_points_for_trapz(points, N)
+		self.integrate_coefficients(values, N)
 
-	def get_points_for_trapz(self, points, N, L):
+	def get_points_for_trapz(self, points, N):
 		"""Convert an array of [t, x] points to be ready for integration
 		Output is a 2D array with rows [t, d, a_1, ..., a_n, b_1, ..., b_n],
 		   where each row corresponds to the value of the integrand at point t
@@ -28,24 +29,36 @@ class Fourier:
 
 		d = xs * 0.5
 
-		a_n = np.array([xs * np.cos(n * ts * np.pi / L) for n in range(1, N+1)])
+		a_n = np.array([xs * np.cos(2 * n * ts * np.pi / self.L) for n in range(1, N+1)])
 
-		b_n = np.array([xs * np.sin(n * ts * np.pi / L) for n in range(1, N+1)])
+		b_n = np.array([xs * np.sin(2 * n * ts * np.pi / self.L) for n in range(1, N+1)])
 
 		integrand_values = np.array([ts, d, *a_n, *b_n])
 
 		return integrand_values
 
+
 	def integrate_coefficients(self, integrand_values, N):
 		ts, values = integrand_values[0, :], integrand_values[1:, 0:]
 
 		coeffs = np.trapz(values, x=ts, axis=1)
+		coeffs *= (2 / self.L)
 
-		d = coeffs[0]
-		a_n = coeffs[1:1+N]
-		b_n = coeffs[1+N:]
+		self.d = coeffs[0]
+		self.a_n = coeffs[1:1+N]
+		self.b_n = coeffs[1+N:]
 
-		return d, a_n, b_n
+
+	def __call__(self, t):
+		"""Evaluate the fourier series f(t) at a time t"""
+		f = 0
+		f += self.d
+
+		n = np.arange(1, len(self.a_n)+1)
+		f += self.a_n * np.cos(2 * n * t * np.pi / self.L)
+		f += self.b_n * np.sin(2 * n * t * np.pi / self.L)
+
+		return f
 
 
 if __name__ == "__main__":
@@ -53,6 +66,3 @@ if __name__ == "__main__":
 		[1, 2],
 		[4, 3]
 	])
-
-	L = points[-1, 0] - points[0, 0]
-	get_points_for_trapz(points, 5, L)
