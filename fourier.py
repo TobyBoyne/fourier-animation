a04c94cde78016f0b0acc244ecea5a9bd23ed962
Toby Boyne <tobyboyne@gmail,.com>
2020-03-07 15:07:13 +0000

    Converted functions into Fourier class

--- fourier.py
+++ fourier.py
@@ -4,36 +4,48 @@ Calculates the Fourier coefficients for a given set of data points
 
 import numpy as np
 
-def get_points_for_trapz(points, N, L):
-	"""Convert an array of [t, x] points to be ready for integration
-	Output is a 2D array with rows [t, d, a_1, ..., a_n, b_1, ..., b_n],
-	   where each row corresponds to the value of the integrand at point t
-	These rows can then be integrated across via the trapezium rule
 
-	This will create rows up to the Nth coefficient of the Fourier series"""
+class Fourier:
+	def __init__(self, points, N):
+		self.d = 0
+		self.a_n = np.zeros(N)
+		self.b_n = np.zeros(N)
 
-	ts = points[:, 0]
-	xs = points[:, 1]
+		L = points[-1, 0] - points[0, 0]
 
-	d = xs * 0.5
+		self.get_poitns_for_trapz(points, L)
 
-	a_n = np.array([xs * np.cos(n * ts * np.pi / L) for n in range(1, N+1)])
+	def get_points_for_trapz(self, points, N, L):
+		"""Convert an array of [t, x] points to be ready for integration
+		Output is a 2D array with rows [t, d, a_1, ..., a_n, b_1, ..., b_n],
+		   where each row corresponds to the value of the integrand at point t
+		These rows can then be integrated across via the trapezium rule
 
-	b_n = np.array([xs * np.sin(n * ts * np.pi / L) for n in range(1, N+1)])
+		This will create rows up to the Nth coefficient of the Fourier series"""
 
-	integrand_values = np.array([ts, d, *a_n, *b_n])
-	print(integrand_values)
-	print(integrate_coefficients(integrand_values, N))
-	return integrand_values
+		ts = points[:, 0]
+		xs = points[:, 1]
 
-def integrate_coefficients(integrand_values, N):
-	ts, values = integrand_values[0, :], integrand_values[1:, 0:]
+		d = xs * 0.5
 
-	coeffs = np.trapz(values, x=ts, axis=1)
+		a_n = np.array([xs * np.cos(n * ts * np.pi / L) for n in range(1, N+1)])
 
-	d = coeffs[0]
-	a_n = coeffs[1:1+N]
-	b_n = coeffs[1+N:]
+		b_n = np.array([xs * np.sin(n * ts * np.pi / L) for n in range(1, N+1)])
+
+		integrand_values = np.array([ts, d, *a_n, *b_n])
+
+		return integrand_values
+
+	def integrate_coefficients(self, integrand_values, N):
+		ts, values = integrand_values[0, :], integrand_values[1:, 0:]
+
+		coeffs = np.trapz(values, x=ts, axis=1)
+
+		d = coeffs[0]
+		a_n = coeffs[1:1+N]
+		b_n = coeffs[1+N:]
+
+		return d, a_n, b_n
 
 
 if __name__ == "__main__":
