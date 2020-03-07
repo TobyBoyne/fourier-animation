1b5765172138b3dc0f7e45818a50ff92b5360c28
Toby Boyne <tobyboyne@gmail,.com>
2020-03-07 13:57:15 +0000

    Data now recorded; stops recording upon mouse release

--- draw.py
+++ draw.py
@@ -21,18 +21,30 @@ class Drawer:
 		self.record_data = False
 		self.start_time = 0
 
-		click_id =	fig.canvas.mpl_connect('button_press_event', self.draw_line)
+		click_id =	fig.canvas.mpl_connect('button_press_event', self.start_plotting)
 		move_id =	fig.canvas.mpl_connect('motion_notify_event', self.plot_point)
+		stop_id =	fig.canvas.mpl_connect('button_release_event', self.stop_plotting)
 
-	def draw_line(self, event):
+	def start_plotting(self, event):
 		"""Start recording data"""
 		self.record_data = True
 		self.start_time = perf_counter()
 
 	def plot_point(self, event):
+		"""Plot a point at the current mouse (x, y) position
+		Record the time at which this point was plotted
+		If the mouse has not yet been clicked, or the cursor is outside of axes, don't plot a point"""
 		if self.record_data and event.inaxes:
-			ax.plot(event.xdata, event.ydata, marker="x")
+			x, y = event.xdata, event.ydata
+			ax.plot(x, y, marker="x")
 			plt.draw()
+
+			t = perf_counter() - self.start_time
+			self.points = np.append(self.points, [t, x, y])
+
+	def stop_plotting(self, event):
+		"""Stop recording data"""
+		self.record_data = False
 
 if __name__ == "__main__":
 	fig, ax = plt.subplots()
