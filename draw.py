970145a2b46e8d43f8b569c25832664d7af2f56b
Toby Boyne <tobyboyne@gmail,.com>
2020-03-07 13:33:11 +0000

    Added code to get data from clicking on figure

--- draw.py
+++ draw.py
@@ -8,8 +8,22 @@ Uses matplotlib.pyplot events:
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Drawer:
 	def __init__(self, fig):
 		self.fig = fig
+		self.points = np.array([])
+		self.record_data = False
 
+	def draw_line(self, event):
+		print(event.xdata)
+		
+
+fig, ax = plt.subplots()
+ax.plot([1,2,3,4,5])
+draw = Drawer(fig)
+user_input = fig.canvas.mpl_connect('button_press_event', draw.draw_line)
+plt.show()
+# while True:
+# 	draw.draw_line() # here you click on the plot
