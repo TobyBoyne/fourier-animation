2be32e047048adf8b8c982c14ea74ad53bf0b7c6
Toby Boyne <tobyboyne@gmail,.com>
2020-03-17 22:15:27 +0000

    Changed main to display multiple Fourier approximations at once, side by side

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from anim import Animator
 
 if __name__ == "__main__":
 	# number of coefficients in Fourier series
-	N = 10
+	Ns = (2, 12)
 
 	# --- user input ---
 	fig, ax = plt.subplots()
@@ -18,22 +18,29 @@ if __name__ == "__main__":
 	plt.show()
 
 	# --- find Fourier series for drawn shape ---
-	fourier = Fourier(draw.points, N)
+	fouriers = [Fourier(draw.points, N) for N in Ns]
 
 	# --- plot drawn shape against Fourier approximation ---
+	fig, axs = plt.subplots(1, len(Ns))
 	ps = draw.points
 	t_start, t_end = ps[0, 0].real, ps[-1, 0].real
 	ts = np.linspace(t_start, t_end, 100)
-	plt.plot(ps[:, 1].real, ps[:, 1].imag)
 
-	f = fourier(ts)
-	plt.plot(f.real, f.imag)
+	fs = [f(ts) for f in fouriers]
+	for ax, f in zip(axs, fs):
+		ax.plot(ps[:, 1].real, ps[:, 1].imag)
+		ax.plot(f.real, f.imag)
 
 	plt.legend(("User Input", "Fourier Approximation"))
 	plt.show()
 
 	# --- animate Fourier drawing ---
-	anim_fig, anim_ax = plt.subplots()
-	anim = Animator(anim_fig, anim_ax, fourier, ts[-1])
-	anim.save('gifs\drawing.gif', writer='imagemagick', fps=30)
+	anim_fig, anim_axs = plt.subplots(1, len(Ns))
+	anim = None
+	for anim_ax, fourier in zip(anim_axs, fouriers):
+		anim_ax.set_xlim([0, 1])
+		anim_ax.set_ylim([0, 1])
+		anim = Animator(anim_fig, anim_ax, fourier, ts[-1])
+
 	plt.show()
+	anim.save('gifs\drawing.gif', writer='imagemagick', fps=30)
